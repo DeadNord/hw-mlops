5cafa403169b6febfcf295a244d998d5c74466e6
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2025-09-08 22:45:27 +0200

    refactor agrocd by example

--- agrocd/terraform.tf
+++ agrocd/terraform.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = ">= 1.5"
+  required_version = ">= 1.0"
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,26 +12,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.11"
+      version = "~> 2.10"
     }
-  }
-}
-
-provider "aws" {
-  region  = var.region
-  profile = var.profile
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
